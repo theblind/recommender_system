725e1ba41396a105880d163f6c594cc8b647f803
Kyle <ruanbowen@gmail.com>
2018-03-23 15:30:30 +0800

    Move get_start.sh to datasets

--- datasets/get_start.sh
+++ datasets/get_start.sh
@@ -4,7 +4,4 @@ hash wget 2>/dev/null || { echo >&2 "Wget required.  Aborting."; exit 1; }
 hash unzip 2>/dev/null || { echo >&2 "unzip required.  Aborting."; exit 1; }
 
 wget http://files.grouplens.org/datasets/movielens/ml-20m.zip
-unzip -o "ml-latest.zip"
-DESTINATION="./datasets/"
-mkdir -p $DESTINATION
-mv ml-latest $DESTINATION
+unzip -o "ml-20m.zip"
